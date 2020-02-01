6f6a0532bb92be79b10a7a7d36238dceddc3e93a
Yago <yago.alvarado@gmail.com>
2020-02-01 08:09:59 +0000

    added gitignore file and completed first use case

---
+++ JG.FinTechTest.Tests/Services/GiftAidCalculatorServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using JG.FinTechTest.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JG.FinTechTest.Tests.Services
+{
+    [TestFixture]
+    class GiftAidCalculatorServiceTests
+    {
+        IGiftAidCalculatorService _giftAidCalculator = new GiftAidCalculatorService();
+
+        [TestCase(100, 25)]
+        [TestCase(111, 27.75)]
+        public void ShouldCalculateTheCorrectGiftAid(decimal donatedAmount, decimal expectedGiftAid)
+        {
+            decimal calculatedGiftAid = this._giftAidCalculator.Calculate(donatedAmount);
+
+            Assert.That(calculatedGiftAid, Is.EqualTo(expectedGiftAid));
+        }
+    }
+}
---
+++ JG.FinTechTest/Services/GiftAidCalculatorService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JG.FinTechTest.Services
+{
+    public interface IGiftAidCalculatorService
+    {
+        decimal Calculate(decimal donatedAmount);
+    }
+
+    public class GiftAidCalculatorService : IGiftAidCalculatorService
+    {
+        private const decimal BASIC_TAX_RATE = 20m;
+        private readonly decimal _taxRate;
+        private readonly decimal _giftAidRate;
+
+        public GiftAidCalculatorService():this(BASIC_TAX_RATE)
+        {}
+
+        public GiftAidCalculatorService(decimal taxRate)
+        {
+            this._taxRate = taxRate;
+            this._giftAidRate = this._taxRate / (100m - this._taxRate);
+        }
+        public decimal Calculate(decimal donatedAmount)
+        {
+            return donatedAmount * this._giftAidRate;
+        }
+    }
+}
